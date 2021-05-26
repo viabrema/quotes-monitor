3025eab6b28e25c3d7c9b0f1c98cfdce69c1b7d3
viabrema <viabrema@gmail.com>
2021-05-26 09:34:07 -0300

    favicon

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -22,7 +22,7 @@ function Login({ onLogin }) {
                             Olá <strong>Rafael</strong>, <strong>Gabriel</strong>, ou qualquer outro interessado nessa aplicação que esteja lendo esse texto. Esse monitor de cotações foi desenvolvido para fins didáticos ao mostrar um pouco do meu conhecimento prático com React.
                         </p>
                         <p>
-                            Esse sistema foi levantado em aproximadamente 12h e trata-se de um monitor de cotações do mercado financeiro em tempo real,  utilizando uma Fake API que gera valores de dados aleatórios com uma grid de cotações em tempo real que interage diretamente com um gráfico desenvolvido manualmente em SVG.
+                            Esse sistema foi levantado em aproximadamente 12 horas e trata-se de um monitor de cotações de mercado financeiro em realtime,  utilizando uma Fake API que gera valores de dados aleatórios com uma grid de cotações em tempo real que interage diretamente com um gráfico desenvolvido manualmente em SVG.
                         </p>
                     </li>)}
 
